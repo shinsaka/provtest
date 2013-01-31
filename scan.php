3022a1b1aac2a330589e6a77999c7de4596800b1
manabu.shinsaka <shinx1265@gmail.com>
2013-01-31 23:36:42 +0900

    scan :

--- scan.php
+++ scan.php
@@ -10,16 +10,22 @@
 <?php
 $start_time = microtime(true);
 
-$count = 0;
-$fp = fopen('/mnt/output/test.txt', 'r');
-while (($buf = fgets($fp)) !== false) {
-    if (substr($buf, 9, 1) == '0') {
-        $count++;
+$scan_code = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ,'a', 'b', 'c', 'd', 'e', 'f');
+
+foreach ($scan_code as $value) {
+    $count = 0;
+    $fp = fopen('/mnt/output/test.txt', 'r');
+    while (($buf = fgets($fp)) !== false) {
+        if (substr($buf, 9, 1) == $value) {
+            $count++;
+        }
     }
-}
-fclose($fp);
+    fclose($fp);
 ?>
-<h1>10桁目が0の行は、<?php echo $count ?>行ありました。</h1>
+<h1>10桁目が<?php echo $value ?>の行は、<?php echo $count ?>行ありました。</h1>
+<?php
+}
+?>
 
 <div>処理時間は<?php echo microtime(true) - $start_time ?>秒でした</div>
 
